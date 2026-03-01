a70c4514861ebef4d86b742fbdbba926940f08d4
vv7x-x <cvcvccvc48@gmail.com>
2026-03-01 17:43:03 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // مهم جداً عشان يعمل static export
+
+  images: {
+    unoptimized: true, // GitHub Pages لا يدعم Image Optimization
+  },
+
+  // لو الريبو مش باسم username.github.io
+  // غير "repo-name" باسم الريبو بتاعك
+  basePath: "/rr",
+  assetPrefix: "/rr/",
 };
 
 export default nextConfig;
